c6ad73ae8a69ff45a16f2e7bdcfbd92d2d7bd589
Nouf Almutairi <noufn8988@gmail.com>
2025-11-11 16:03:22 +0300

    Create shell.h

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <unistd.h>
+
+/**
+ * _putchar - write one character to stdout
+ * @c: character to print
+ *
+ * Return: 1 on success, or -1 on error.
+ */
+int _putchar(char c);
+
+#endif /* SHELL_H */
