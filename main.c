2dd1cb959b122c11d3f5b579b398ca5d3536456e
Nouf Almutairi <noufn8988@gmail.com>
2025-11-11 16:08:23 +0300

    Update main.c

--- main.c
+++ main.c
@@ -9,10 +9,10 @@
  */
 int main(int argc, char **argv)
 {
- (void)argc;
+(void)argc;
  (void)argv;
 
  /* Placeholder only to keep repo Betty-clean for Task 1 */
- _putchar('\n');
- return (0);
+_putchar('\n');
+return (0);
 }
