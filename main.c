86715999488e936d5db28b46ceff6ce907821d40
Nouf Almutairi <noufn8988@gmail.com>
2025-11-11 16:04:41 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * main - Entry point for the simple shell project (placeholder for next tasks)
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char **argv)
+{
+ (void)argc;
+ (void)argv;
+
+ /* Placeholder only to keep repo Betty-clean for Task 1 */
+ _putchar('\n');
+ return (0);
+}
