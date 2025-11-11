d28ae9713d76ad0a36b8ea87c68772a2b7c7a559
Nouf Almutairi <noufn8988@gmail.com>
2025-11-11 16:04:16 +0300

    Create _putchar.c

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+ * _putchar - write one character to stdout
+ * @c: character to print
+ *
+ * Return: 1 on success, or -1 on error.
+ */
+int _putchar(char c)
+{
+ return (write(1, &c, 1));
+}
