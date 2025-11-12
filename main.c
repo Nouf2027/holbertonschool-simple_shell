8a651e8956dbe4a9a9a7aabfdd2a048149c650a1
Nouf Almutairi <noufn8988@gmail.com>
2025-11-12 13:01:21 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,89 +5,67 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 extern char **environ;
-
 static void print_prompt(void)
 {
-write(STDOUT_FILENO, "$ ", 2);
+write(STDOUT_FILENO,"$ ",2);
 }
-
 static void strip_newline(char *s)
 {
 size_t n;
-if (!s)
-return;
-n = strlen(s);
-if (n && s[n - 1] == '\n')
-s[n - 1] = '\0';
+if(!s)return;
+n=strlen(s);
+if(n&&s[n-1]=='\n')s[n-1]='\0';
 }
-
-static char *trim_spaces(char *s)
+static char *ltrim_rtrim(char *s)
 {
-size_t i, j, n;
-if (!s)
-return s;
-n = strlen(s);
-i = 0;
-while (i < n && (s[i] == ' ' || s[i] == '\t'))
-i++;
-j = n;
-while (j > i && (s[j - 1] == ' ' || s[j - 1] == '\t'))
-j--;
-s[j] = '\0';
-return s + i;
+size_t i=0,j,n;
+if(!s)return s;
+n=strlen(s);
+while(i<n&&(s[i]==' '||s[i]=='\t'))i++;
+j=n;
+while(j>i&&(s[j-1]==' '||s[j-1]=='\t'))j--;
+s[j]='\0';
+return s+i;
 }
-
-static int has_space(const char *s)
+static int make_argv(char *line,char **argv,int cap)
 {
-size_t i;
-if (!s)
-return 0;
-for (i = 0; s[i]; i++)
-if (s[i] == ' ' || s[i] == '\t')
-return 1;
-return 0;
+int c=0;
+char *tok=strtok(line," \t");
+while(tok&&c<cap-1){argv[c++]=tok;tok=strtok(NULL," \t");}
+argv[c]=NULL;
+return c;
 }
-
 int main(void)
 {
-char *line = NULL, *cmd, *argv[2];
-size_t cap = 0;
-ssize_t nread;
-int interactive, status;
+char *line=NULL,*work,*argv[64];
+size_t cap=0;
+ssize_t n;
+int interactive,status,argc;
 pid_t pid;
-interactive = isatty(STDIN_FILENO);
-while (1)
+interactive=isatty(STDIN_FILENO);
+while(1)
 {
-if (interactive)
-print_prompt();
-nread = getline(&line, &cap, stdin);
-if (nread == -1)
+if(interactive)print_prompt();
+n=getline(&line,&cap,stdin);
+if(n==-1)
 {
-if (interactive)
-write(STDOUT_FILENO, "\n", 1);
+if(interactive)write(STDOUT_FILENO,"\n",1);
 break;
 }
 strip_newline(line);
-cmd = trim_spaces(line);
-if (!*cmd)
-continue;
-if (has_space(cmd))
+work=ltrim_rtrim(line);
+if(!*work)continue;
+argc=make_argv(work,argv,64);
+if(argc==0)continue;
+pid=fork();
+if(pid==-1)continue;
+if(pid==0)
 {
-dprintf(STDERR_FILENO, "%s: not found\n", cmd);
-continue;
-}
-pid = fork();
-if (pid == -1)
-continue;
-if (pid == 0)
-{
-argv[0] = cmd;
-argv[1] = NULL;
-execve(cmd, argv, environ);
-perror(cmd);
+execve(argv[0],argv,environ);
+perror(argv[0]);
 _exit(127);
 }
-waitpid(pid, &status, 0);
+waitpid(pid,&status,0);
 }
 free(line);
 return 0;
